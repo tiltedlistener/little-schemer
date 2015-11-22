49e5330abf7f40d4417e178d337a84073e8cd805
tiltedlistener <tiltedlistener@gmail.com>
2015-11-22 15:53:15 -0800

    First page

--- chapter5.scm
+++ chapter5.scm
@@ -1,2 +1,16 @@
+(define atom?
+  (lambda (x)
+    (and (not (pair? x)) (not (null? x)))))
 
 
+(define rember*
+  (lambda (a lat)
+    (cond
+     ((null? lat) (quote()))
+     ((atom? (car lat))
+	     (cond
+	      ((eq? a (car lat)) (rember* a (cdr lat)))
+	      (else
+	       (cons (car lat) (rember* a (cdr lat))))))
+     (else
+      (cons (rember* a (car lat)) (rember* a (cdr lat)))))))
