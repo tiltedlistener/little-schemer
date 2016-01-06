fe98778384bc5a8be8ea0d9ba2bcccbec1128ee4
tiltedlistener <tiltedlistener@gmail.com>
2016-01-05 18:58:21 -0800

    Little more of chap 8

--- chapter8.scm
+++ chapter8.scm
@@ -30,6 +30,7 @@
 
 (define rember-eq? (rember-f eq?))
 
+;; These are incorrect. Sorry but lazy copying happened. 
 (define insertL
   (lambda (new old lat)
     (cond
@@ -59,3 +60,65 @@
 
 (define insertL-eq? (insertL-f eq?))
 
+
+;; Starting page 132
+
+; Original Methods
+(define insertL
+  (lambda (new old lat)
+    (cond
+     ((null? lat) (quote()))
+     ((eq? (car lat) old)
+      (cons new lat))
+      (else
+       (cons (car lat) (insertl new old (cdr lat)))))))
+
+(define insertR
+  (lambda (new old lat)
+    (cond
+     ((null? lat) (quote()))
+     ((eq? (car lat) old)
+      (cons (car lat) (cons new (cdr lat))))
+      (else
+       (cons (car lat) (insertR new old (cdr lat)))))))
+
+; Sequences
+(define seqR
+  (lambda (new old l)
+     (cons old (cons new (cdr l)))))
+
+(define seqL
+  (lambda (new old l)
+    (cons new l)))
+
+(define insert-g
+  (lambda (seq)
+    (lambda (new old l)
+      (cond
+       ((null? l) (quote()))
+       ((eq? old (car l)) (seq new old l))
+       (else
+	(cons (car l) ((insert-g seq) new old (cdr l))))))))
+
+(define insertL (insert-g seqL))
+(define insertR (insert-g seqR))
+
+; Subst
+(define subst
+  (lambda (new old lat)
+    (cond
+     ((null? lat) (quote()))
+     ((eq? (car lat) old)
+      (cons new (cdr lat)))
+      (else
+       (cons (car lat) (subst new old (cdr lat)))))))
+
+(define seqS
+  (lambda (new old l)
+    (cons new (cdr l))))
+
+(define subst (insert-g seqS))
+
+(define seqrem
+  (lambda (new old l)
+    l))
