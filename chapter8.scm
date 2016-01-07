dfd9c00c63cc18f9bd90c4177d809da30f85a9dd
tiltedlistener <tiltedlistener@gmail.com>
2016-01-06 22:02:40 -0800

    Multirember&co

--- chapter8.scm
+++ chapter8.scm
@@ -1,3 +1,8 @@
+(define atom?
+  (lambda (x)
+    (and (not (pair? x)) (not (null? x)))))
+
+
 (define rember?
   (lambda (a lat)
     (cond
@@ -119,6 +124,55 @@
 
 (define subst (insert-g seqS))
 
-(define seqrem
-  (lambda (new old l)
-    l))
+(define multirember
+  (lambda (a lat)
+    (cond
+     ((null? lat) (quote()))
+     ((eq? a (car lat)) (multirember a (cdr lat)))
+     (else
+      (cons (car lat) (multirember a (cdr lat)))))))
+
+(define multirember-f
+  (lambda (test?)
+    (lambda (a lat)
+      (cond
+       ((null? lat) (quote()))
+       ((test? a (car lat)) ((multirember-f test?) a (cdr lat)))
+       (else
+	(cons (car lat) ((multirember-f test?) a (cdr lat))))))))
+
+(define multirember-eq (multirember-f eq?))
+
+(define eq-tuna?
+  (lambda (a)
+    (eq? 'tuna a)))
+
+(define multiremberT
+  (lambda (test?)
+    (lambda (lat)
+      (cond
+       ((null? lat) (quote()))
+       ((test? (car lat)) ((multiremberT test?) (cdr lat)))
+       (else
+	(cons (car lat) ((multiremberT test?) (cdr lat))))))))
+
+
+(define multirember-tuna? (multiremberT eq-tuna?))
+
+(define multirember&co
+  (lambda (a lat col)
+    (cond
+    ((null? lat) (col '() '()))
+    ((eq? (car lat) a)
+     (multirember&co a (cdr lat)
+		     (lambda (newlat seen)
+		       (col newlat (cons (car lat) seen)))))
+    (else
+     (multirember&co a (cdr lat)
+		     (lambda (newlat seen)
+		       (col (cons (car lat) newlat) seen)))))))
+
+(define a-friend
+  (lambda (x y)
+    (null? y)))
+
