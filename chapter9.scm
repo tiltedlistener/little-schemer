d764db8f10829f81035352ccc32c2d4fb6f63abb
tiltedlistener <tiltedlistener@gmail.com>
2016-02-24 23:01:33 -0800

    Final parts of chapter 9

--- chapter9.scm
+++ chapter9.scm
@@ -20,3 +20,53 @@
      (else
       (keep-looking a (pick init lat) lat)))))
 
+					; For page 153
+
+(define atom?
+  (lambda (x)
+    (and (not (pair? x)) (not (null? x)))))
+
+(define first
+  (lambda (p)
+    (car p)))
+
+(define second
+  (lambda (p)
+    (car (cdr p))))
+
+(define build
+  (lambda (s1 s2)
+    (cons s1 (cons s2 '()))))
+
+(define a-pair?
+  (lambda (x)
+    (cond
+     ((atom? x) #f)
+     ((null? x) #f)
+     ((null? (cdr x)) #f)
+     ((null? (cdr (cdr x))) #t)
+     (else #f))))
+
+(define shift
+  (lambda (pair)
+    (build (first (first pair))
+	   (build (second (first pair)) (second pair)))))
+
+(define align
+  (lambda (pora)
+    (cond
+     ((atom? pora) pora)
+     ((a-pair? (first pora)) (align (shift pora)))
+     (else
+      (build (first pora) (align (second pora)))))))
+
+(define length*
+  (lambda (pora)
+    (cond
+     ((atom? pora) 1)
+     (else
+      (+ (length* (first pora)) (length* (second pora)))))))
+     
+(define revpair
+  (lambda (pair)
+    (build (second pair) (first pair))))
